57482d77627a76cf8a34956b8b01479abc0f74a5
David Hickey <david.hickey@ds.mpg.de>
2019-12-17 14:04:56 +0100

    Fixed wrong method signature

--- config.hpp
+++ config.hpp
@@ -9,16 +9,32 @@
 class Config {
 
 public:
+    template <class T>
+    T get(const std::string& section, const std::string& key) const {
+        const char* c_string_value = this->ini.GetValue(section.c_str(), key.c_str(), NULL);
+
+        if (c_string_value == NULL) {
+            std::stringstream error_ss;
+            error_ss << "No such key: '" << section << "/" << key << "'";
+            throw std::invalid_argument(error_ss.str());
+        }
+
+        const std::string string_value(c_string_value);
+        const T value = this->convert_numeric<T>(string_value);
+
+        return value;
+    }
+
     int get_int(const std::string& section, const std::string& key) const {
-        return get<int>(section, key, this->ini);
+        return this->get<int>(section, key);
     }
 
     double get_double(const std::string& section, const std::string& key) const {
-        return get<double>(section, key, this->ini);
+        return this->get<double>(section, key);
     }
 
     std::string get_string(const std::string& section, const std::string& key) const {
-        return get<std::string>(section, key, this->ini);
+        return this->get<std::string>(section, key);
     }
 
     std::string get_file_path() const {
@@ -38,22 +54,6 @@ public:
 private:
     const std::string file_path;
     CSimpleIniA ini;
-
-    template <class T>
-    T get(const std::string& section, const std::string& key, const CSimpleIniA& ini) const {
-        const char* c_string_value = ini.GetValue(section.c_str(), key.c_str(), NULL);
-
-        if (c_string_value == NULL) {
-            std::stringstream error_ss;
-            error_ss << "No such key: '" << section << "/" << key << "'";
-            throw std::invalid_argument(error_ss.str());
-        }
-
-        const std::string string_value(c_string_value);
-        const T value = convert_numeric<T>(string_value);
-
-        return value;
-    }
 
     inline bool file_exists(const std::string& file_path) const {
         struct stat buffer;
