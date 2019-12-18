13386d7c21e49ade90f206ddb046332988d4f425
David Hickey <david.hickey@ds.mpg.de>
2019-12-18 14:20:41 +0100

    Elimited redundancy from bounding box class

--- boundingbox.hpp
+++ boundingbox.hpp
@@ -8,29 +8,26 @@ class BoundingBox {
 
 public:
     BoundingBox(const double x_min, const double x_max, const double y_min, const double y_max, const double z_min, const double z_max)
-    : x_min(x_min), x_max(x_max)
-    , y_min(y_min), y_max(y_max)
-    , z_min(z_min), z_max(z_max)
-    , lower_bounds{x_min, y_min, z_min}
+    : lower_bounds{x_min, y_min, z_min}
     , upper_bounds{x_max, y_max, z_max} {}
 
     BoundingBox(const double x_dim, const double y_dim, const double z_dim)
-    : x_min(-x_dim / 2.0), x_max(x_dim / 2.0)
-    , y_min(-y_dim / 2.0), y_max(y_dim / 2.0)
-    , z_min(-z_dim / 2.0), z_max(z_dim / 2.0)
-    , lower_bounds{x_min, y_min, z_min}
-    , upper_bounds{x_max, y_max, z_max} {}
+    : lower_bounds{-x_dim / 2.0, -y_dim / 2.0, -z_dim / 2.0}
+    , upper_bounds{+x_dim / 2.0, +y_dim / 2.0, +z_dim / 2.0} {}
+
+    BoundingBox(const BoundingBox& other)
+    : lower_bounds(other.get_lower_bounds())
+    , upper_bounds(other.get_upper_bounds()) {}
 
     template <class T>
-    inline bool in_bounds(const T x, const T y, const T z) const {
-        return (x > this->x_min && x < this->x_max)
-            && (y > this->y_min && y < this->y_max)
-            && (z > this->z_min && z < this->z_max);
-    };
+    inline bool in_bounds(const MathArray<T, 3>& position) const {
+        for (size_t i = 0; i < 3; ++i) {
+            if (position[i] < this->lower_bounds[i] || position[i] > this->upper_bounds[i]) {
+                return false;
+            }
+        }
 
-    template <class T>
-    inline bool in_bounds(const MathArray<T, 3>& point) const {
-        return this->in_bounds(point[0], point[1], point[2]);
+        return true;
     };
 
     template <class T>
@@ -69,33 +66,44 @@ public:
     }
 
     inline MathArray<double, 3> random_point_in_bounds(std::mt19937& engine) const {
-        return MathArray<double, 3>{
-            std::uniform_real_distribution<double>(this->x_min, this->x_max)(engine),
-            std::uniform_real_distribution<double>(this->y_min, this->y_max)(engine),
-            std::uniform_real_distribution<double>(this->z_min, this->z_max)(engine)
-        };
+        MathArray<double, 3> output{};
+
+        for (size_t i = 0; i < 3; ++i) {
+            output[i] = std::uniform_real_distribution<double>(this->lower_bounds[i], this->upper_bounds[i])(engine);
+        }
+
+        return output;
     }
 
     inline double volume() const {
-        return (this->x_max - this->x_min)
-             * (this->y_max - this->y_min)
-             * (this->z_max - this->z_min);
+        double volume = 1;
+
+        for (size_t i = 0; i < 3; ++i) {
+            volume *= (this->upper_bounds[i] - this->lower_bounds[i]);
+        }
+
+        return volume;
     }
 
-    inline double get_xmin() const { return this->x_min; };
-    inline double get_xmax() const { return this->x_max; };
-    inline double get_ymin() const { return this->y_min; };
-    inline double get_ymax() const { return this->y_max; };
-    inline double get_zmin() const { return this->z_min; };
-    inline double get_zmax() const { return this->z_max; };
-    inline double get_xsize() const { return this->x_max - this->x_min; };
-    inline double get_ysize() const { return this->y_max - this->y_min; };
-    inline double get_zsize() const { return this->z_max - this->z_min; };
+    inline const MathArray<double, 3>& get_upper_bounds() const {
+        return this->upper_bounds;
+    }
+
+    inline const MathArray<double, 3>& get_lower_bounds() const {
+        return this->lower_bounds;
+    }
+
+    inline double get_xmin() const { return this->lower_bounds[0]; };
+    inline double get_xmax() const { return this->upper_bounds[0]; };
+    inline double get_ymin() const { return this->lower_bounds[1]; };
+    inline double get_ymax() const { return this->upper_bounds[1]; };
+    inline double get_zmin() const { return this->lower_bounds[2]; };
+    inline double get_zmax() const { return this->upper_bounds[2]; };
+    inline double get_xsize() const { return this->upper_bounds[0] - this->lower_bounds[0]; };
+    inline double get_ysize() const { return this->upper_bounds[1] - this->lower_bounds[1]; };
+    inline double get_zsize() const { return this->upper_bounds[2] - this->lower_bounds[2]; };
 
 private:
-    const double x_min, x_max;
-    const double y_min, y_max;
-    const double z_min, z_max;
     const MathArray<double, 3> lower_bounds;
     const MathArray<double, 3> upper_bounds;
 
