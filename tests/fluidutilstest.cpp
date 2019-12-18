739aa0a83b0fe75278771fdf0f2bca15ad687780
David Hickey <david.hickey@ds.mpg.de>
2019-12-18 11:29:02 +0100

    Added test for translating flow solution

--- tests/fluidutilstest.cpp
+++ tests/fluidutilstest.cpp
@@ -33,15 +33,75 @@ void test_blake() {
     // Curried function to test blake tensor.
     auto blake_tester = curry_blake_tester(bb, real_sphere_location, shear_viscosity);
 
-    // Test with a nice x-parallel stokes drag
+    // Test boundary condition
     assert(all(abs(blake_tester(MathArray<double, 3>{-10, 0, 0}, MathArray<double, 3>{-1, 0, 0}) - MathArray<double, 3>{0, 0, 0}) < 1e-12), "Failed blake tensor 1");
     assert(all(abs(blake_tester(MathArray<double, 3>{3, -8, 0}, MathArray<double, 3>{-1, 0, 0}) - MathArray<double, 3>{0, 0, 0}) < 1e-12), "Failed blake tensor 2");
     assert(all(abs(blake_tester(MathArray<double, 3>{3, -8, 0}, MathArray<double, 3>{-1, 5, 0}) - MathArray<double, 3>{0, 0, 0}) < 1e-12), "Failed blake tensor 3");
-    // assert(blake_tester(MathArray<double, 3>{+3, -8, 0}, MathArray<double, 3>{-1, 0, 0}) - MathArray<double, 3>{0, 0, 0}, "Failed blake tensor 2", 1e-7);
-    // assert(blake_tester(MathArray<double, 3>{+3, -8, 0}, MathArray<double, 3>{-1, +5, -6}) - MathArray<double, 3>{0, 0, 0}, "Failed blake tensor 3", 1e-7);
+}
+
+void test_translation() {
+    const double z = 10;
+    const MathArray<double, 3> sphere_position{0, 0, z};
+    double radius = 1;
+
+    // Test 1: if sphere radius is 0 then should be no change.
+    {
+        const MathArray<double, 3> unperturbed_velocity{1, 4, -13};
+        const MathArray<double, 3> trans = translating_flow_at(MathArray<double, 3>{1, 1, 1}, sphere_position, unperturbed_velocity, 0);
+        assert_all_eq(trans, unperturbed_velocity, "Failed radius 0 check");
+    }
+
+    // Test 2: should be symmetric around flow axis.
+    {
+        const double x_offset = -10;
+        const MathArray<double, 3> unperturbed_velocity{1, 0, 0};
+
+        // Offset to symmetry axis, diametrically opposite
+        const MathArray<double, 3> trans_above = translating_flow_at(MathArray<double, 3>{x_offset, 0, z+5}, sphere_position, unperturbed_velocity, radius);
+        const MathArray<double, 3> trans_below = translating_flow_at(MathArray<double, 3>{x_offset, 0, z-5}, sphere_position, unperturbed_velocity, radius);
+
+        // Along symmetry axis
+        const MathArray<double, 3> trans_middle = translating_flow_at(MathArray<double, 3>{x_offset, 0, z}, sphere_position, unperturbed_velocity, radius);
+
+        assert(trans_above[0] == trans_below[0] && trans_above[1] == -trans_below[1] && trans_above[2] == -trans_below[2], "Failed symmetry condition 1a");
+        assert(trans_middle[1] == 0 && trans_middle[2] == 0, "Failed symmetry condition 1c");
+    }
+
+    // Test 3: should be symmetric around flow axes regardless of which axis it is and how they are diametrically opposite.
+    {
+        const double y_offset = -10;
+        const MathArray<double, 3> unperturbed_velocity{0, 1, 0};
+
+        // Offset to symmetry axis, diametrically opposite
+        const MathArray<double, 3> trans_above = translating_flow_at(MathArray<double, 3>{5, y_offset, z+5}, sphere_position, unperturbed_velocity, radius);
+        const MathArray<double, 3> trans_below = translating_flow_at(MathArray<double, 3>{-5, y_offset, z-5}, sphere_position, unperturbed_velocity, radius);
+
+        // Along symmetry axis
+        const MathArray<double, 3> trans_middle = translating_flow_at(MathArray<double, 3>{0, y_offset, z}, sphere_position, unperturbed_velocity, radius);
+
+        assert(trans_above[0] == -trans_below[0] && trans_above[1] == trans_below[1] && trans_above[2] == -trans_below[2], "Failed symmetry condition 2a");
+        assert(trans_middle[0] == 0 && trans_middle[2] == 0, "Failed symmetry condition 2c");
+    }
+
+    // Test 4: non-slip boundary
+    {
+        const MathArray<double, 3> unperturbed_velocity{0, 1, 0};
+
+        for (size_t index = 0; index < 3; ++index) {
+            for (int plusminus : MathArray<int, 2>{+1, -1}) {
+                MathArray<double, 3> surface_point(sphere_position);
+                surface_point[index] += plusminus * radius;
+
+                const auto flow = translating_flow_at(surface_point, sphere_position, unperturbed_velocity, radius);
+
+                assert(all(abs(flow) < 1e-12), "Failed non-slip surface condition");
+            }
+        }
+    }
 }
 
 int main() {
     test_stokes_drag();
     test_blake();
+    test_translation();
 }
