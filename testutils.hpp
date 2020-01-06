09906ee7ee84e967fc2a22a75865b55af099ff8b
David Hickey <david.hickey@ds.mpg.de>
2020-01-06 13:38:19 +0100

    Added 'assert_all_approx_eq' test utility function

--- testutils.hpp
+++ testutils.hpp
@@ -16,3 +16,10 @@ void assert_all_eq(const MathArray<T, N>& a1, const MathArray<T, N>& a2, const s
         assert(a1[i] == a2[i], message);
     }
 }
+
+template <class T, size_t N>
+void assert_all_approx_eq(const MathArray<T, N>& a1, const MathArray<T, N>& a2, const double tol, const std::string& message) {
+    for (size_t i = 0; i < N; ++i) {
+        assert(std::abs(a1[i] - a2[i]) < tol, message);
+    }
+}
