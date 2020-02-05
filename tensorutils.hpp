9cc8e5c82d1edf7ea3ba6ec4770ed6e44b1c4cf9
David Hickey <david.hickey@ds.mpg.de>
2020-02-05 15:44:06 +0100

    Added new ways to access elements

--- tensorutils.hpp
+++ tensorutils.hpp
@@ -1,13 +1,66 @@
 #include <cstddef>
 #include <stdexcept>
 
+template <class T, size_t N, size_t M> class Tensor;
 
 struct Index {
     size_t i, j;
 };
 
 template <class T, size_t N, size_t M>
+struct ConstRow {
+    const Tensor<T, N, M>* tensor;
+    const size_t index;
+
+    const T& operator[](const size_t i) const {
+        return tensor->data[tensor->flatten(index, i)];
+    }
+
+    const T& at(const size_t i) const {
+        if (i >= M) {
+            throw std::range_error("Indices out of bounds for tensor");
+        }
+
+        return (*this)[i];
+    }
+};
+
+template <class T, size_t N, size_t M>
+struct Row {
+    Tensor<T, N, M>* tensor;
+    const size_t index;
+
+    T& operator[](const size_t i) {
+        return tensor->data[tensor->flatten(index, i)];
+    }
+
+    const T& operator[](const size_t i) const {
+        return tensor->data[tensor->flatten(index, i)];
+    }
+
+    T& at(const size_t i) {
+        if (i >= M) {
+            throw std::range_error("Indices out of bounds for tensor");
+        }
+
+        return (*this)[i];
+    }
+
+    const T& at(const size_t i) const {
+        if (i >= M) {
+            throw std::range_error("Indices out of bounds for tensor");
+        }
+
+        return (*this)[i];
+    }
+};
+
+
+template <class T, size_t N, size_t M>
 class Tensor {
+
+    friend class Row<T, N, M>;
+    friend class ConstRow<T, N, M>;
 
 private:
     size_t flatten(size_t i, size_t j) const {
@@ -21,6 +74,8 @@ private:
 public:
     T data[N * M];
 
+    // 2 ways to access tensor data: with Index struct or using Row proxy
+    // object.
     T& operator[](const Index& i) {
         return this->data[this->flatten(i)];
     }
@@ -29,20 +84,56 @@ public:
         return this->data[this->flatten(i)];
     }
 
+    Row<T, N, M> operator[](const size_t i) {
+        return Row<T, N, M>{this, i};
+    }
+
+    ConstRow<T, N, M> operator[](const size_t i) const {
+        ConstRow<T, N, M> out{this, i};
+
+        return out;
+    }
+
+    // 3 ways to access tensor data: Index struct, Row proxy, or just passing
+    // two indices.
     T& at(const Index& i) {
-        if (i.i >= N || i.j >= M) {
+        return this->at(i.i, i.j);
+    }
+
+    const T& at(const Index& i) const {
+        return this->at(i.i, i.j);
+    }
+
+    Row<T, N, M> at(const size_t i) {
+        if (i >= N) {
             throw std::range_error("Indices out of bounds for tensor");
         }
 
         return (*this)[i];
     }
 
-    const T& at(const Index& i) const {
-        if (i.i >= N || i.j >= M) {
+    ConstRow<T, N, M> at(const size_t i) const {
+        if (i >= N) {
             throw std::range_error("Indices out of bounds for tensor");
         }
 
         return (*this)[i];
+    }
+
+    T& at(const size_t i, const size_t j) {
+        if (i >= N || j >= M) {
+            throw std::range_error("Indices out of bounds for tensor");
+        }
+
+        return this->data[this->flatten(i, j)];
+    }
+
+    const T& at(const size_t i, const size_t j) const {
+        if (i >= N || j >= M) {
+            throw std::range_error("Indices out of bounds for tensor");
+        }
+
+        return this->data[this->flatten(i, j)];
     }
 };
 
