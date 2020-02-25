35f1ba80b8629883ef60c9be20c33b7dfa4856b3
David Hickey <david.hickey@ds.mpg.de>
2020-02-25 15:23:10 +0100

    Added several noexcept specifiers

--- mathutils.hpp
+++ mathutils.hpp
@@ -9,42 +9,42 @@
 #include <cmath>
 
 
-inline constexpr int levicevita(const int i, const int j, const int k) {
+inline constexpr int levicevita(const int i, const int j, const int k) noexcept {
     return (i - j) * (j - k) * (k - i) / 2;
 }
 
 
-inline constexpr int delta(const int i, const int j) {
+inline constexpr int delta(const int i, const int j) noexcept {
     return int(i == j);
 }
 
-inline constexpr std::array<size_t, 2> unflatten_bin_index(const size_t index, const size_t bins_x, const size_t bins_y) {
+inline constexpr std::array<size_t, 2> unflatten_bin_index(const size_t index, const size_t bins_x, const size_t bins_y) noexcept {
     return std::array<size_t, 2>{
         index / bins_y, // x index
         index % bins_y  // y index
     };
 }
 
-inline constexpr size_t flatten_bin_index(const size_t index_x, const size_t index_y, const size_t bins_x, const size_t bins_y) {
+inline constexpr size_t flatten_bin_index(const size_t index_x, const size_t index_y, const size_t bins_x, const size_t bins_y) noexcept {
     return index_x * bins_y + index_y;
 }
 
 template <class T>
-inline constexpr T square(const T x) {
+inline constexpr T square(const T x) noexcept {
     return x * x;
 }
 
 template <class T>
-inline constexpr T cube(const T x) {
+inline constexpr T cube(const T x) noexcept {
     return x * x * x;
 }
 
-inline constexpr double hypot(const double a, const double b) {
+inline constexpr double hypot(const double a, const double b) noexcept {
     return std::sqrt(square(a) + square(b));
 }
 
 template <class T, int N>
-inline constexpr T power(const T x) {
+inline constexpr T power(const T x) noexcept {
     T out = 1;
 
     for (size_t i = 0; i < N; ++i) {
